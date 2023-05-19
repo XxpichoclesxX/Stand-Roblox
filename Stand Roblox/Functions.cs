7668e10a5b550a74eefb2ba406416b8c4b4c1684
XxpichoclesxX <81063926+XxpichoclesxX@users.noreply.github.com>
2023-05-19 14:20:18 -0600

    Add files via upload

---
+++ Stand Roblox/Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stand_Roblox
+{
+    class Functions
+    {
+
+        public static void PopulateListBox(ListBox lsb, string Folder, string FileType)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            FileInfo[] Files = dinfo.GetFiles(FileType);
+            foreach (FileInfo file in Files)
+            {
+                {
+                    lsb.Items.Add(file.FullName);
+                }
+            }
+        }
+    }
+}
